5bc559bf6e08f6b7def792c56d296a79e0c81f8d
BegaliyevXushnud <xushnudpubgmn1@gmail.com>
2024-11-13 11:24:21 +0500

    ads

--- app/component/header.tsx
+++ app/component/header.tsx
@@ -73,7 +73,7 @@ const [selectedCategory, setSelectedCategory] = useState("Smartfonlar va Aksessu
            </div>
             </div>
             <div className=' flex items-center justify-between p-2 2xl:p-0 ' >
-                <div className='flex gap-1 items-center p-2'>
+                <div className='flex gap-1 items-center '>
                     <Image src={headerlogo} priority alt='ashyo' className='w-[48px] h-[48px]' />
                     <Image src={headertextimg} priority alt='ashyotext'  className='w-[102px] h-[42px]'/>
                 </div>
